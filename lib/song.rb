7f251d9820a5fac423adf7beb099a12fad25a12d
Daniel Matney <danieljmatney@gmail.com>
2019-10-05 03:48:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,9 +49,7 @@ class Song
     song = self.new
     new_name = fullname.split("- ")[1]
     song.name = new_name.split(".mp3")[0]
-    puts song.name
     song.artist_name = fullname.split(" -")[0]
-    puts song.artist_name
     @@all << song
   end
     
