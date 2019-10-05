b4c872e3d5a4d9eab4ba4053b775a216b64a8587
Daniel Matney <danieljmatney@gmail.com>
2019-10-05 03:09:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,9 @@ class Song
   end
   
   def self.find_by_name(name)
-    self.all.find{|song| song.name == name}
+    self.all.find{|song| song.name == name
+      return song.name
+    }
   end
   
   def self.find_or_create_by_name(name)
